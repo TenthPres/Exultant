be7753a3b894078bcf065f8cef73c17e84ee00fa
James K <jkrrv@jkrrv.org>
2021-09-25 08:39:42 -0400

    header improvements; starting xhr search

--- assets/js/tenth-defer.js
+++ assets/js/tenth-defer.js
@@ -8,3 +8,39 @@ if (!!document.getElementById('pageProgressBarProgress')) {
         document.getElementById("pageProgressBarProgress").style.width = scrolled + "%";
     })
 }
+
+
+// xhr Search
+if (!!document.getElementById('search-form-1')) {
+    let input = document.getElementById('search-form-1'),
+        debounceTimer = null,
+        lastSearchedValue = null,
+        xhr = null;
+
+    function handleXhrResponse() {
+        console.log(this.responseText);
+    }
+
+    function doXhrSearch() {
+        let searchTerm = input.value.trim()
+        if (searchTerm === lastSearchedValue || debounceTimer !== null) {
+            return;
+        }
+        if (xhr !== null) {
+            xhr.abort();
+        }
+        // TODO handle cases of "" value
+        xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', handleXhrResponse);
+        xhr.open("GET", `/wp-json/wp/v2/search?search=${searchTerm}`);
+        lastSearchedValue = searchTerm;
+        xhr.send();
+    }
+
+    input.addEventListener('keyup', function() {
+        if (debounceTimer === null) {
+            debounceTimer = setTimeout(function() { debounceTimer = null; doXhrSearch() }, 700);
+            doXhrSearch();
+        }
+    })
+}
