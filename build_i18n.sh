0ae4eec5ef67feeab02009f65ea82862ab501495
James K. <jkrrv@jkrrv.org>
2025-02-27 18:03:56 -0500

    Attempting to fix build

--- build_i18n.sh
+++ build_i18n.sh
@@ -5,6 +5,9 @@ if [ ! -f wp-cli.phar ]; then
     wget -O wp-cli.phar https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 fi
 
+chmod +x wp-cli.phar
+
+mkdir -p ./build/i18n
 cp -r ./i18n ./build/i18n
 
 php ./wp-cli.phar i18n make-json ./build/i18n
