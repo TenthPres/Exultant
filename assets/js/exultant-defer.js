b35303093f62291d5542786c525bb448ebf479c1
James K. <jkrrv@jkrrv.org>
2025-04-04 14:27:44 -0400

    rescope search js

--- assets/js/exultant-defer.js
+++ assets/js/exultant-defer.js
@@ -12,164 +12,175 @@ if (!!document.getElementById('pageProgressBarProgress')) {
 
 
 // xhr Search
-if (!!document.getElementById('search-input')) {
-    let input = document.getElementById('search-input'),
-        ul = document.getElementById('search-results-list'),
-        statusSpan = document.getElementById('search-results-status'),
-        statusLi = statusSpan.parentElement,
-        debounceTimer = null,
-        lastSearchedValue = "",
-        xhr = null;
+function initSearch() {
+    const input = document.getElementById('search-input');
+    const ul = document.getElementById('search-results-list');
+    const statusSpan = document.getElementById('search-results-status');
+    const statusLi = statusSpan.parentElement;
+    let debounceTimer = null;
+    let lastSearchedValue = "";
+    let xhr = null;
 
-    document.addEventListener('keyup', function(event) {
-        if (event.key === "/" &&
-            document.activeElement?.tagName.toString().toUpperCase() !== "INPUT" &&
-            document.activeElement?.tagName.toString().toUpperCase() !== "TEXTAREA") {
-            input.focus();
-            event.preventDefault();
-        }
-    });
-    input.addEventListener("blur", SearchResult.prototype.clearActiveRes);
-    input.addEventListener("focus", doXhrSearch);
-
-    function SearchResult(data, ul) {
-        this.a = document.createElement('a');
-        let li = document.createElement('li'),
-            exc = document.createElement('div'),
-            a = this.a,
-            res = this;
-        a.className = "";
-        a.href = data.url;
-        a.innerHTML = data.title;
-        li.appendChild(a);
-        if (data.excerpt?.trim().length > 0) {
-            exc.innerHTML = data.excerpt;
-            li.appendChild(exc);
-        }
-        ul.appendChild(li);
-
-        a.addEventListener('mouseover', () => res.setActiveRes());
-        a.addEventListener('mouseout', SearchResult.prototype.clearActiveRes);
-
-        SearchResult.prototype.results.push(this);
-
-        this.setActiveRes = function() {
-            SearchResult.prototype.clearActiveRes();
-            this.a.classList.add('selected');
-            SearchResult.prototype.selectedResult = this;
-        }
-    }
-    SearchResult.prototype.clearActiveRes = function() {
-        if (SearchResult.prototype.selectedResult) {
-            SearchResult.prototype.selectedResult.a.classList.remove('selected');
-            SearchResult.prototype.selectedResult = null;
-        }
-    }
-    SearchResult.prototype.advanceSelectedResult = function(steps) {
-        let i = 0;
-        if (SearchResult.prototype.results.length < 1) {
-            SearchResult.prototype.clearActiveRes();
-            return;
-        }
-        if (SearchResult.prototype.selectedResult) {
-            i = SearchResult.prototype.results.indexOf(SearchResult.prototype.selectedResult);
-        } else if (steps > 0) {
-            i--;
-        }
-        i += steps + SearchResult.prototype.results.length; // adding the length prevents negative indexes
-        i %= SearchResult.prototype.results.length;
-        SearchResult.prototype.results[i].setActiveRes();
-    }
-    SearchResult.prototype.selectedResult = null;
-    SearchResult.prototype.results = [];
-
-    function clearSearchResults() {
-        while (ul.lastChild !== statusLi) {
-            ul.removeChild(ul.lastChild)
-        }
-        statusSpan.innerText = __("Start Typing...", "Exultant");
-        statusLi.style.display = "";
-    }
-
-    function handleXhrResponse() {
-        let data = JSON.parse(this.responseText);
-
-        clearSearchResults();
-
-        if (data.length === 0 && lastSearchedValue.trim().length > 0) {
-            statusSpan.innerText = __("No Results", "Exultant");
-        } else {
-            statusLi.style.display = "none";
-        }
-
-        SearchResult.prototype.results = [];
-
-        // add new results
-        for (di in data) {
-            new SearchResult(data[di], ul)
-        }
-    }
-
-    function doXhrSearch() {
-        let searchTerm = input.value.trim()
-        if (searchTerm.length === 0) {
-            clearSearchResults();
-            lastSearchedValue = "";
-            return
-        }
-        if (searchTerm === lastSearchedValue || debounceTimer !== null) {
-            return;
-        }
-        statusSpan.innerText = __("Loading...", "Exultant");
-        statusLi.style.display = "";
-        if (xhr !== null) {
-            xhr.abort();
-        }
-        xhr = new XMLHttpRequest();
-        xhr.addEventListener('load', handleXhrResponse);
-        xhr.open("GET", `/wp-json/wp/v2/search?search=${searchTerm}`);
-        lastSearchedValue = searchTerm;
-        xhr.send();
-    }
-
-    function searchInputEvent() {
-        if (debounceTimer === null) {
-            debounceTimer = setTimeout(function() { debounceTimer = null; doXhrSearch() }, 700);
-            doXhrSearch();
-        }
-    }
-
-    input.addEventListener('input', searchInputEvent);
-    input.addEventListener('keyup', function(event) {
-        searchInputEvent();
-
-        switch(event.key) {
-            case "ArrowDown":
-                SearchResult.prototype.advanceSelectedResult(1);
+    if (input) {
+        document.addEventListener('keyup', (event) => {
+            if (event.key === "/" &&
+                !["INPUT", "TEXTAREA"].includes(document.activeElement?.tagName.toUpperCase())) {
+                input.focus();
                 event.preventDefault();
-                break;
+            }
+        });
 
-            case "ArrowUp":
-                SearchResult.prototype.advanceSelectedResult(-1);
-                event.preventDefault();
-                break;
+        class SearchResult {
+            constructor(data, ul) {
+                this.a = document.createElement('a');
+                const li = document.createElement('li');
+                const exc = document.createElement('div');
+                this.a.href = data.url;
+                this.a.innerHTML = data.title;
+                li.appendChild(this.a);
+                if (data.excerpt?.trim().length > 0) {
+                    exc.innerHTML = data.excerpt;
+                    li.appendChild(exc);
+                }
+                ul.appendChild(li);
 
-            case "Escape":
-                document.body.focus();
-                input.value = "";
-                doXhrSearch(); // cancels existing XHRs.
-                clearSearchResults();
-                lastSearchedValue = "";
-                event.preventDefault();
-                break;
+                this.a.addEventListener('mouseover', () => this.setActiveRes());
+                this.a.addEventListener('mouseout', SearchResult.clearActiveRes);
 
-            case "Enter":
-                if (SearchResult.prototype.selectedResult) {
-                    window.location = SearchResult.prototype.selectedResult.a.href;
-                    event.preventDefault();
+                SearchResult.results.push(this);
+            }
+
+            setActiveRes() {
+                SearchResult.clearActiveRes();
+                this.a.classList.add('selected');
+                SearchResult.selectedResult = this;
+            }
+
+            static clearActiveRes() {
+                if (SearchResult.selectedResult) {
+                    SearchResult.selectedResult.a.classList.remove('selected');
+                    SearchResult.selectedResult = null;
+                }
+            }
+
+            static advanceSelectedResult(steps) {
+                if (SearchResult.results.length < 1) {
+                    SearchResult.clearActiveRes();
                     return;
                 }
-                break;
+                let i = SearchResult.selectedResult ? SearchResult.results.indexOf(SearchResult.selectedResult) : -1;
+                i = (i + steps + SearchResult.results.length) % SearchResult.results.length;
+                SearchResult.results[i].setActiveRes();
+            }
         }
-    })
+
+        input.addEventListener("blur", SearchResult.clearActiveRes);
+        input.addEventListener("focus", doXhrSearch);
+
+        SearchResult.selectedResult = null;
+        SearchResult.results = [];
+
+        function clearSearchResults() {
+            while (ul.lastChild !== statusLi) {
+                ul.removeChild(ul.lastChild);
+            }
+            statusSpan.innerText = __("Start Typing...", "Exultant");
+            statusLi.style.display = "";
+        }
+
+        function handleXhrResponse() {
+            const data = JSON.parse(this.responseText);
+
+            // if results are for an older query, do nothing.
+            if (lastSearchedValue !== this.query) {
+                return;
+            }
+
+            clearSearchResults();
+
+            if (data.length === 0 && lastSearchedValue.trim().length > 0) {
+                statusSpan.innerText = __("No Results", "Exultant");
+            } else {
+                statusLi.style.display = "none";
+            }
+
+            SearchResult.results = [];
+
+            data.forEach(item => new SearchResult(item, ul));
+        }
+
+        function doXhrSearch() {
+            const searchTerm = input.value.trim();
+            if (searchTerm.length === 0) {
+                clearSearchResults();
+                lastSearchedValue = "";
+                return;
+            }
+            if (searchTerm === lastSearchedValue || debounceTimer !== null) {
+                return;
+            }
+            statusSpan.innerText = __("Loading...", "Exultant");
+            statusLi.style.display = "";
+            if (xhr !== null) {
+                xhr.removeEventListener('load', handleXhrResponse);
+                xhr.abort();
+            }
+            xhr = new XMLHttpRequest();
+            xhr.query = searchTerm;
+            xhr.addEventListener('load', handleXhrResponse);
+            xhr.open("GET", `/wp-json/wp/v2/search?search=${searchTerm}&locale=${tpvm.locale}`);
+            lastSearchedValue = searchTerm;
+            xhr.send();
+        }
+
+        function searchInputEvent() {
+            if (debounceTimer === null) {
+                debounceTimer = setTimeout(() => {
+                    debounceTimer = null;
+                    doXhrSearch();
+                }, 700);
+                doXhrSearch();
+            }
+        }
+
+        input.addEventListener('input', searchInputEvent);
+        input.addEventListener('keydown', (event) => {
+            searchInputEvent();
+
+            switch (event.key) {
+                case "ArrowDown":
+                    SearchResult.advanceSelectedResult(1);
+                    event.preventDefault();
+                    break;
+
+                case "ArrowUp":
+                    SearchResult.advanceSelectedResult(-1);
+                    event.preventDefault();
+                    break;
+
+                case "Escape":
+                    if (input.value === "") {
+                        event.preventDefault();
+                        document.body.focus();
+                        input.blur();
+                        break;
+                    } else {
+                        input.value = "";
+                        doXhrSearch(); // cancels existing XHRs.
+                        clearSearchResults();
+                        lastSearchedValue = "";
+                        event.preventDefault();
+                        break;
+                    }
+
+                case "Enter":
+                    if (SearchResult.selectedResult) {
+                        window.location = SearchResult.selectedResult.a.href;
+                    }
+                    event.preventDefault();
+                    break;
+            }
+        });
+    }
 }
+initSearch();
